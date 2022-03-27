f7dce7ba3429f2b4abcda0354879a9d184a2167b
GandalfTheHipster <38801722+GandalfTheHipster@users.noreply.github.com>
2022-03-27 22:34:51 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="container-fluid text-center">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<a class="navbar-brand" href="#"><span class="JOSH">JOSH</span> <span class="orange">WHITKIN</span></a>
+
+</div>
+<div class="col-md-6">
+    <p>Terms and Conditons</p>
+    
+    </div>
+</div> <!-- row -->
+</div> <!-- container -->
+</footer>
+<?php get_footer();?>
+
+</body>
+</html>
