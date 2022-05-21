29592fe055cda779505ca831777a11c8405afc43
GandalfTheHipster <38801722+GandalfTheHipster@users.noreply.github.com>
2022-05-21 17:27:24 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>"> 
+  <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="Description" content="Enter your description here"/>
@@ -13,27 +13,30 @@
 
 
 
-<title>Title<?php
+<title><?php
   if (function_exists('is_tag') && is_tag()) {
-    single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
- elseif (is_archive()) {
-    wp_title(''); echo ' Archive - '; }
- elseif (is_search()) {
-    echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
- elseif (!(is_404()) && (is_single()) || (is_page())) {
-    wp_title(''); echo ' - '; }
- elseif (is_404()) {
-    echo 'Not Found - '; }
- if (is_home()) {
-    bloginfo('name'); echo ' - '; bloginfo('description'); }
- else {
-     bloginfo('name'); }
- if ($paged>1) {
-    echo ' - page '. $paged; }
+     single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+  elseif (is_archive()) {
+     wp_title(''); echo ' Archive - '; }
+  elseif (is_search()) {
+     echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+  elseif (!(is_404()) && (is_single()) || (is_page())) {
+     wp_title(''); echo ' - '; }
+  elseif (is_404()) {
+     echo 'Not Found - '; }
+  if (is_home()) {
+     bloginfo('name'); echo ' - '; bloginfo('description'); }
+  else {
+      bloginfo('name'); }
+  if ($paged>1) {
+     echo ' - page '. $paged; }
 ?>
+
+
 </title>
 <meta name="description" content="<?php bloginfo('description'); ?>">
 <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
+
 </head>
 <body>
 <header class="container-fluid bg1">
@@ -70,5 +73,5 @@
       <h1>Games Design For The Future</h1>
     </div>
     </div> <!-- container -->
-    <?php wp_head(); ?>
+    <?php wp_head(); ?> 
     </header>
