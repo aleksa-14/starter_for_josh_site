3d8e7fe87c65da30d3a910042bed1aa88c27d0e8
GandalfTheHipster <38801722+GandalfTheHipster@users.noreply.github.com>
2022-05-21 22:39:45 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,12 +2,15 @@
 <div class="container">
 <div class="row">
 <div class="col-md-6">
-<a class="navbar-brand" href="#"><span class="JOSH">JOSH</span> <span class="orange">WHITKIN</span></a>
+<!-- <a class="navbar-brand" href="#"><span class="JOSH">JOSH</span> <span class="orange">WHITKIN</span></a> -->
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
 
 </div>
 <div class="col-md-6">
-    <p>Terms and Conditons</p>
-    
+    <!-- <p>Terms and Conditons</p> -->
+    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
     </div>
 </div> <!-- row -->
 </div> <!-- container -->
